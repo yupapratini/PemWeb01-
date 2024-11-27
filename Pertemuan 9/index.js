88a9fbcd155b1d31bc4c9544547800af0553b283
yupapratini <yulfapratini15@gmail.com>
2024-11-27 14:01:45 +0700

    Add files via upload

---
+++ Pertemuan 9/index.js
@@ -0,0 +1,4 @@
+//let nama = " Yupa"  "Yupa": unknown word.
+let nama = document.getElementById('nama')
+
+nama 
---
+++ Pertemuan 9/luaslingkaran.js
@@ -0,0 +1,17 @@
+// deklarasi dan inisialisasi varibel jari-jari dengan nilai 15
+let jari2 = 15    
+
+// deklarasi dan inisialisasi variabel jari-jari dengan nilai 3.13
+const PHI = 3.14
+
+// meghitung keliling  lingkaran dan menyimpannya ke dalam variabel keliling
+let kelilig = 2 * PHI * jari2
+
+// menhitung luas lingkaran dan menyimpannya ke dalam  variabel luas
+let luas = PHI * jari2 * jari2
+
+
+document.write("Keliling lingkaran dengan jari-jari " + 
+jari2 + " = " + keliling);
+document.write("<br>");
+document.write("Sedangkan luasnya = " + luas);
